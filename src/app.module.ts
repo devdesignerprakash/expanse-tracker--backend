9bc8f216b410c04fc52afbe60086110d492cdbc6
devdesignerprakash <devdesignerprakash@gmail.com>
2026-04-21 20:48:09 +0545

    wip:auth api on progress

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,14 @@ import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { dbConfig } from './database/database.config';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true,
     envFilePath: '.env',
     load: [dbConfig]
-  }),DatabaseModule, UserModule],
+  }),DatabaseModule, UserModule,AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,7 @@
+import { Controller } from "@nestjs/common";
+
+
+@Controller('auth')
+export class AuthController {
+  // Implement authentication endpoints here
+}
